923b69552d5ae8b8e4a079bc73e510b921a827fb
karthiktr147 <karthiktr147@gmail.com>
2020-04-07 07:17:32 +0530

    "8"

--- Brand1/src/main/java/com/mindtree/brand/Brand1Application.java
+++ Brand1/src/main/java/com/mindtree/brand/Brand1Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Brand1Application {
-
+//1
 	public static void main(String[] args) {
 		SpringApplication.run(Brand1Application.class, args);
 	}
